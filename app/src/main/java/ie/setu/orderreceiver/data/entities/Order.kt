98af95b685de55a72ec0a8da77dc54b3776420a0
Conor Coker <conorcoker@gmail.com>
2025-05-07 00:25:58 +0100

    Added google firestore to store orders in firestore. Menu items are still stored in room but when clicked are added to orders which is firestore. These orders can be swiped to be deleted or tapped to be updated i.e completed

---
+++ app/src/main/java/ie/setu/orderreceiver/data/entities/Order.kt
@@ -0,0 +1,11 @@
+package ie.setu.orderreceiver.data.entities
+
+data class Order(
+    //java.lang.RuntimeException: Could not deserialize object. Class ie.setu.orderreceiver.data.entities.Order does not define a no-argument constructor.
+    val itemId: String = "",
+    val name: String = "",
+    val price: Double = 0.0,
+    val completed: Boolean = false,
+    val timestamp: Long = 0L,
+    val userId: String = ""
+)
--- app/src/main/java/ie/setu/orderreceiver/ui/viewmodels/MenuViewModel.kt
+++ app/src/main/java/ie/setu/orderreceiver/ui/viewmodels/MenuViewModel.kt
@@ -1,10 +1,14 @@
 package ie.setu.orderreceiver.ui.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ie.setu.orderreceiver.data.dao.MenuDao
 import ie.setu.orderreceiver.data.entities.MenuItem
+import ie.setu.orderreceiver.data.entities.Order
 import ie.setu.orderreceiver.utils.Categories
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,9 +23,15 @@ class MenuViewModel @Inject constructor(private val dao: MenuDao) : ViewModel() 
     private val _menu = MutableStateFlow<List<MenuItem>>(emptyList())
     val menu: StateFlow<List<MenuItem>> get() = _menu.asStateFlow()
 
+    // Orders (Assignment 2)
+    private val _orders = MutableStateFlow<List<Order>>(emptyList())
+    val orders: StateFlow<List<Order>> get() = _orders.asStateFlow()
+
     init {
         loadMenuItems()
     }
+
+    private val fireStore = FirebaseFirestore.getInstance()
 
     fun loadMenuItems() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -51,5 +61,75 @@ class MenuViewModel @Inject constructor(private val dao: MenuDao) : ViewModel() 
                 _menu.value = menuItems
             }
         }
+    }
+
+    //Firestore (Assignment 2)
+
+    //https://firebase.google.com/docs/firestore/manage-data/add-data#kotlin_1
+    fun addToOrder(menuItem: MenuItem, onAddToOrderSuccess: () -> Unit, onAddToOrderFail: () -> Unit) {
+        val order = Order(
+            itemId = menuItem.itemId,
+            name = menuItem.name,
+            price = menuItem.price,
+            userId = FirebaseAuth.getInstance().currentUser!!.uid
+        )
+        //https://stackoverflow.com/questions/73718391/how-to-add-data-to-the-firebase-firestore-by-generating-different-document-id-af
+        fireStore.collection("orders")
+            .document(order.itemId)
+            .set(order)
+            .addOnSuccessListener {
+                Log.d("MenuViewModel", "Order successfully uploaded!")
+                onAddToOrderSuccess()
+            }
+            .addOnFailureListener { e ->
+                Log.w("MenuViewModel", "Error uploading order", e)
+                onAddToOrderFail()
+            }
+    }
+
+    fun loadOrders() {
+        Log.d("MenuViewModel", "loadOrders() called.")
+        fireStore.collection("orders")
+            .whereEqualTo("userId", FirebaseAuth.getInstance().currentUser!!.uid)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Log.w("MenuViewModel", "Error loading orders", exception)
+                    return@addSnapshotListener
+                }
+
+                val orderList = snapshot?.documents?.map { document ->
+                    document.toObject(Order::class.java) ?: Order(
+                        itemId = "",
+                        name = "",
+                        price = 0.0,
+                        userId = FirebaseAuth.getInstance().currentUser!!.uid
+                    )
+                } ?: emptyList()
+
+                _orders.value = orderList
+            }
+    }
+
+    fun deleteOrder(orderId: String, onSuccess: () -> Unit) {
+        fireStore.collection("orders").document(orderId)
+            .delete()
+            .addOnSuccessListener {
+                Log.d("MenuViewModel", "Order $orderId deleted successfully.")
+                onSuccess()
+            }
+            .addOnFailureListener { e ->
+                Log.e("MenuViewModel", "Error deleting order $orderId", e)
+            }
+    }
+
+    fun updateOrderStatus(orderId: String) {
+        fireStore.collection("orders").document(orderId)
+            .update("completed", true)
+            .addOnSuccessListener {
+                Log.d("MenuViewModel", "Order status updated.")
+            }
+            .addOnFailureListener { e ->
+                Log.e("MenuViewModel", "Error updating order status", e)
+            }
     }
 }
